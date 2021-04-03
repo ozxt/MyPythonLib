a4b426d3ed75a0797c0661a2c30662fbe78acdac
ozxt <1350723639@qq.com>
2021-04-03 09:04:48 +0800

    heap

---
+++ heap.py
@@ -0,0 +1,64 @@
+heap_parent = lambda i:(i-1)//2
+heap_left = lambda i:i*2+1
+heap_right = lambda i:i*2+2
+
+class Heap(object):
+    def __init__(self, compare):
+        super(Heap, self).__init__()
+        self.size = 0
+        self.tree = []
+        self.cmp = compare
+
+    def insert(self, data):
+        self.tree.append(data)
+        ipos = self.size
+        ppos = heap_parent(ipos)
+        tmp = self.tree[ipos]
+        while ipos>0:
+            if self.cmp(tmp, self.tree[ppos]) == 1:
+                self.tree[ipos] = self.tree[ppos]
+                ipos = ppos
+                ppos = heap_parent(ipos)
+            else:
+                break
+        self.tree[ipos] = tmp
+        self.size += 1
+
+
+    def extract(self):
+        if self.size == 0:
+            return None
+
+        data = self.tree[0]
+        self.size -= 1
+        if self.size == 0:
+            self.tree = []
+            return data
+        self.tree[0] = self.tree[self.size]
+        ipos = 0
+        while ipos < self.size:
+            left = heap_left(ipos)
+            right = heap_right(ipos)
+            if left < self.size and self.cmp(self.tree[ipos],self.tree[left]) == -1:
+                mpos = left
+            else:
+                mpos = ipos
+
+            if right < self.size and self.cmp(self.tree[mpos], self.tree[right]) == -1:
+                mpos = right
+
+            if mpos == ipos:
+                break
+            else:
+                self.tree[ipos], self.tree[mpos] = self.tree[mpos], self.tree[ipos]
+                ipos = mpos
+
+        return data
+
+
+def compare(a,b):
+    if a>b:
+        return 1
+    if a==b:
+        return 0
+    return -1
